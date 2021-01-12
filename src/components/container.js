8b5b6e919f7e7915f12c5737acc812d6867544c4
AlexHilson <alexhilsonwork@googlemail.com>
2021-01-12 16:06:52 +0000

    outline

--- src/components/container.js
+++ src/components/container.js
@@ -7,11 +7,13 @@ import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
 
 
-const StyledDiv = styled.div`
+const OuterDiv = styled.div`
   margin: 0 auto;
-  max-width: 600px;
-  padding: ${rhythm(2)};
-  padding-top: ${rhythm(1.5)};
+  max-width: 800px;
+  min-width: 400px;
+
+  background-color: rgba(255,255,255,0.9);
+
 `;
 
 const StyledLink = styled(Link)`
@@ -25,6 +27,17 @@ const HeaderLink = styled.p`
   font-style: normal;
 `
 
+const StyledHeader = styled.header`
+  background-color: rgba(255,255,255,0.95);
+  padding-left: 1rem;
+  padding-right: 1rem;
+`
+
+const InnerDiv = styled.div`
+  padding: ${rhythm(2)};
+  padding-top: ${rhythm(1.5)}
+`
+
 const ListLink = props => (
     <li style={{ display: `inline-block`, marginRight: `1rem`, float: `right` }}>
       <StyledLink to={props.to}>{props.children}</StyledLink>
@@ -33,11 +46,15 @@ const ListLink = props => (
 
 export default function Container({ children }) {
 
-    return <StyledDiv>
-        <header style={{background: "white"}}>
+    return <OuterDiv>
+        <StyledHeader>
         <Link to="/"><HeaderLink><h3>sensewithdata</h3></HeaderLink></Link>
-        <Link to="/about/"><HeaderLink style={{ float: `right`}}><h4>about</h4></HeaderLink></Link>
-        </header>
-        {children}
-    </StyledDiv>
+        <ListLink to="/about/"><HeaderLink style={{ float: `right`}}><h4>whatnot</h4></HeaderLink></ListLink>
+        <ListLink to="/about/"><HeaderLink style={{ float: `right`}}><h4>about</h4></HeaderLink></ListLink>
+        </StyledHeader>
+        <InnerDiv>
+          {children}
+          </InnerDiv>
+
+    </OuterDiv>
 }
