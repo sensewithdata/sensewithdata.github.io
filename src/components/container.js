feff09241415c312a4a8d8521d242bbb80f96cf2
AlexHilson <alexhilsonwork@googlemail.com>
2021-01-26 00:04:36 +0000

    reduce width

--- src/components/container.js
+++ src/components/container.js
@@ -9,7 +9,7 @@ import { rhythm } from "../utils/typography"
 
 const OuterDiv = styled.div`
   margin: 0 auto;
-  max-width: 1000px;
+  max-width: 600px;
   min-width: 200px;
   background-color: rgba(255,255,255,0.97);
 `;
