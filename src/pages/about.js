1873d6f2bf21f749cf57936dfc8b19033f103e81
AlexHilson <alexhilsonwork@googlemail.com>
2021-01-29 03:01:06 +0000

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,9 @@ import Container from "../components/container"
 export default function About() {
   return (
     <Container>
-      <div>wow</div>
+      <p>Created by Natasha Nicholson, Pete Quinn Davis, Annie Legge, and Alex Hilson. As part of our work as Data Fellows in the <a href="https://www.swctn.org.uk/data/">South West Creative Technology Network</a></p>
+
+      <p>This site is under construction :) More soon</p>
     </Container>
   )
 }
