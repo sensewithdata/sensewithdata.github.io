06756e05e5fc76e17f48cc9954c9ca784b7e165a
AlexHilson <alexhilsonwork@googlemail.com>
2021-02-26 05:09:19 +0000

    rollback

--- src/pages/questions.js
+++ src/pages/questions.js
@@ -12,14 +12,11 @@ export default function Home({ data }) {
   <Container>
     <ContentBlock>
     <p>What is a burning question? One that needs to be answered! Taking a prompt from Rebecca Solnit's <a href="https://lithub.com/to-break-the-story-you-must-break-the-status-quo/">"To Break the Story, You Must Break the Status Quo"</a>, we set out to hold a virtual panel discussion that thinks about and beyond the data status quo. What are the dominant narratives? Where are the gaps existing data practice? What's worrying and what's hopeful?</p>
-
-    <p><b>Question 1:</b> <a href="#1">What does a bright / dark data future look like?</a></p>
-    <p><b>Question 2:</b> <a href="#2">How can system mapping data become a springboard for collective action?</a></p>
     </ContentBlock>
 
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <ContentBlock>
-      <div id={node.frontmatter.index} dangerouslySetInnerHTML={{__html: node.html }} />
+      <div dangerouslySetInnerHTML={{__html: node.html }} />
       </ContentBlock>
     ))}
   </Container>
